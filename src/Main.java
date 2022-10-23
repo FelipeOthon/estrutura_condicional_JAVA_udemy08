9490dc5064b171a1afb619326fa9e94fa2f3a481
FelipeOthon <felipeosantiago@gmail.com>
2022-10-23 15:02:07 -0300

    enviar exercicio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,45 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+
+        int peca1, peca2, numeroPecas1, numeroPecas2;
+        double valorPeca1, valorPeca2;
+
+        System.out.println("Digite codigo da peça 1: ");
+        peca1 = scanner.nextInt();
+
+        System.out.printf("Digite o valor da peça: ");
+        valorPeca1 = scanner.nextDouble();
+
+        System.out.println("Digite a quantidade de peças a ser comprado: ");
+        numeroPecas1 = scanner.nextInt();
+
+        System.out.println("Digite codigo da peça 1: ");
+        peca2 = scanner.nextInt();
+
+        System.out.printf("Digite o valor da peça: ");
+        valorPeca2 = scanner.nextDouble();
+
+        System.out.println("Digite a quantidade de peças a ser comprado: ");
+        numeroPecas2 = scanner.nextInt();
+
+
+        double totalPagar = numeroPecas1 * valorPeca1 + numeroPecas2 * valorPeca2;
+
+        System.out.printf("O valor total a pagar é: %.2f", totalPagar);
+
+
+
+
+
+
+
+        scanner.close();
     }
 }
